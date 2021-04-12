7cfa61720b6f2dec36677fe69c545b229281bdb2
Murilo-Luciano <murilo.luciano123@gmail.com>
2021-04-12 15:36:11 -0400

    Create chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
+import chessLayer.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello git!");
-
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
